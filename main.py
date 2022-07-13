8c8ff184cd749e6b4dd03f241938013a0407c5ce
DParkKeebeck <duncan.park@keebeckalpha.com>
2022-07-13 14:24:53 -0400

    test

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from yahooquery import Ticker
 SPY_Holdings = main_etf('SPY')
 display(SPY_Holdings['Weight'])
 
+# test
+
 tickers = Ticker(list(SPY_Holdings.index[:20]), group_by='symbol', asynchronous=True, retry=20, status_forcelist=[404, 429, 500, 502, 503, 504])
 data = tickers.history(period='2y')
 display(data)
